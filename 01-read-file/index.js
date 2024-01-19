12782a82e1faa4accc3639c913ce0d9464defb02
Artyom (Ympakaloky) <Ympakaloky@gmail.com>
2024-01-19 11:49:09 +0500

    feat: 01-read-file is done

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const readableStream = fs.createReadStream(
+  path.join(__dirname, 'text.txt'),
+  'utf-8',
+);
+let data = '';
+readableStream.on('data', (chunk) => (data += chunk));
+readableStream.on('end', () => console.log(data));
