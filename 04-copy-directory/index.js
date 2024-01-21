863aaa18410e48ce6282b2f53b42462a66d79130
Artyom (Ympakaloky) <Ympakaloky@gmail.com>
2024-01-21 19:52:13 +0500

    fix: 04-copy-directory, problem with new files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,31 +1,64 @@
-const fs = require('fs');
 const path = require('path');
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-  if (err) {
-    return console.error(err);
+const fs = require('fs');
+
+const oldAssets = path.join(__dirname, 'files');
+const newAssets = path.join(__dirname, 'files-copy');
+
+fs.readdir(path.join(__dirname), (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      if (path.join(__dirname, file) === path.join(__dirname, 'files-copy')) {
+        fs.rm(newAssets, { recursive: true }, (err) => {
+          if (err) console.log(err);
+          else {
+            forCopy(oldAssets, newAssets);
+          }
+        });
+      }
+    });
   }
-  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+});
+
+async function forCopy(copied, created) {
+  try {
+    await fs.access(created);
+    await fs.rm(created, { recursive: true });
+  } catch (err) {}
+
+  await fs.mkdir(newAssets, { recursive: true }, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+  });
+
+  await fs.readdir(copied, (err, files) => {
     if (err) console.log(err);
     else {
       files.forEach((file) => {
-        fs.stat(path.join(__dirname, 'files', file), (err, stats) => {
+        fs.stat(copied + `\\${file}`, (err, stats) => {
           if (err) {
             console.error(err);
             return;
           }
           if (stats.isFile()) {
-            fs.copyFile(
-              path.join(__dirname, 'files', file),
-              path.join(__dirname, 'files-copy', file),
-              (err) => {
-                if (err) {
-                  console.log('Error Found:', err);
-                }
-              },
-            );
+            fs.copyFile(`${copied}\\${file}`, `${created}\\${file}`, (err) => {
+              if (err) {
+                console.log('Error Found:', err);
+              }
+            });
+          } else if (stats.isDirectory()) {
+            fs.mkdir(`${created}\\${file}`, { recursive: true }, (err) => {
+              if (err) {
+                return console.error(err);
+              }
+            });
+
+            forCopy(`${copied}\\${file}`, `${created}\\${file}`);
           }
         });
       });
     }
   });
-});
+}
+forCopy(oldAssets, newAssets);
